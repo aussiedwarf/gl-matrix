a1df1ad5ecec2b1dc58b24bb83573bd4855bbaa9
Eden Harris <aussiedwarf@gmail.com>
2016-02-24 12:29:36 +1100

    Merge float mat4 to dmat4

--- src/gl-matrix/dmat4.js
+++ src/gl-matrix/dmat4.js
@@ -1233,6 +1233,14 @@ dmat4.fromRotationTranslationScaleOrigin = function (out, q, v, s, o) {
   return out;
 };
 
+/**
+ * Calculates a 4x4 matrix from the given quaternion
+ *
+ * @param {dmat4} out mat4 receiving operation result
+ * @param {dquat} q Quaternion to create matrix from
+ *
+ * @returns {dmat4} out
+ */
 dmat4.fromQuat = function (out, q) {
     var x = q[0], y = q[1], z = q[2], w = q[3],
         x2 = x + x,
